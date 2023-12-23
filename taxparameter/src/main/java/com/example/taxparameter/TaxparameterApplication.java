3dcaf9a0efad8e08303bc904b2a881b18fefac28
fxywhs <130030067+fxywhs@users.noreply.github.com>
2023-12-23 21:21:45 +0700

    Add files via upload

---
+++ taxparameter/src/main/java/com/example/taxparameter/TaxparameterApplication.java
@@ -0,0 +1,13 @@
+package com.example.taxparameter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxparameterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaxparameterApplication.class, args);
+	}
+
+}
---
+++ taxparameter/src/test/java/com/example/taxparameter/TaxparameterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.taxparameter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxparameterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
